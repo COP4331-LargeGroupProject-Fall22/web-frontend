11e5583d548d5c6b7afe68ad28b4d0551f51ce87
Nashir Janmohamed <nashirbass@gmail.com>
2022-11-10 15:59:00 -0500

    Implement first version of generic modal that we can reuse for all modal instances

---
+++ src/components/ModalView.vue
@@ -0,0 +1,60 @@
+<template>
+  <div @click="handleBackdropClick">
+    <button @click="open = true">Open Modal</button>
+
+    <!-- Wrapper for modal -->
+    <div v-if="open" id="myModal" class="modal">
+      <!-- Modal content -->
+      <div class="modal-content">
+        <button @click="open = false" class="close">
+          <font-awesome-icon icon="times" />
+        </button>
+        <p>Some text in the Modal..</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      open: false,
+    };
+  },
+  methods: {
+    handleBackdropClick(e) {
+      if (e.target.className == "modal") {
+        this.open = false;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  width: 10%;
+}
+
+.modal {
+  display: block;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0, 0, 0); /* Fallback color */
+  background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+</style>
