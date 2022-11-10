439250be81db6fa6ecbf5deee654fdf76e6fdfcf
BryanVeith <bveith2000@gmail.com>
2022-11-10 18:57:37 -0500

    added styles and buttons and item cards and section cards on ingredient feed file

--- src/components/IngredientFeed.vue
+++ src/components/IngredientFeed.vue
@@ -1,14 +1,108 @@
 <template>
   <div class="container">
-    <header class="jumbotron">
-      <h3>This is the ingredients page</h3>
-    </header>
-
     <!-- Example of how to use MDB rows to open the centered modal view -->
     <!-- <MDBRow center class="text-center">
       <h3>Vue Teleport Example</h3>
       <ModalView />
     </MDBRow> -->
+    <div class="tool-bar">
+      <div class="tool-bar-container">
+        <button type="button" class="btn">Sort By</button>
+      </div>
+      <div class="tool-bar-container">
+        <div class="form-outline">
+          <input
+            type="search"
+            id="form1"
+            class="form-control"
+            placeholder="Search Ingredients"
+            aria-label="Search"
+          />
+        </div>
+        <!-- TODO replace with an actual material icon -->
+        <button type="button" class="btn btn-primary btn-floating">
+          <i class="fas fa-plus"></i>
+        </button>
+      </div>
+    </div>
+    <!-- TODO see if this can be done in a list -->
+    <!-- TODO it automatically moves to a new row, 
+      if we want a sidescroller we need to set up an overflow system, scroll bar or button -->
+    <div class="row">
+      <div class="col-md-auto">
+        <a href="#Dairy" class="btn type-of-food">Diary</a>
+      </div>
+      <div class="col-md-auto">
+        <a href="#Produce" class="btn type-of-food">Produce</a>
+      </div>
+      <div class="col-md-auto">
+        <a href="#Misc1" class="btn type-of-food">Misc1</a>
+      </div>
+      <div class="col-md-auto">
+        <a href="#Misc2" class="btn type-of-food">Misc2</a>
+      </div>
+      <div class="col-md-auto">
+        <a href="#Misc3" class="btn type-of-food">Misc3</a>
+      </div>
+      <div class="col-md-auto">
+        <a href="#Misc4" class="btn type-of-food">Misc4</a>
+      </div>
+      <div class="col-md-auto">
+        <a href="#Misc5" class="btn type-of-food">Misc5</a>
+      </div>
+    </div>
+    <div id="Dairy" class="row">
+      <h3>Diary</h3>
+    </div>
+    <div class="row">
+      <div class="col-md-auto">
+        <button type="button" class="btn food-item">Food</button>
+      </div>
+      <div class="col-md-auto">
+        <button type="button" class="btn food-item">Food</button>
+      </div>
+      <div class="col-md-auto">
+        <button type="button" class="btn food-item">Food</button>
+      </div>
+      <div class="col-md-auto">
+        <button type="button" class="btn food-item">Food</button>
+      </div>
+      <div class="col-md-auto">
+        <button type="button" class="btn food-item">Food</button>
+      </div>
+      <div class="col-md-auto">
+        <button type="button" class="btn food-item">Food</button>
+      </div>
+      <div class="col-md-auto">
+        <button type="button" class="btn food-item">Food</button>
+      </div>
+    </div>
+    <div id="Produce" class="row">
+      <h3>Produce</h3>
+    </div>
+    <div class="row">
+      <div class="col-md-auto">
+        <button type="button" class="btn food-item">Food</button>
+      </div>
+      <div class="col-md-auto">
+        <button type="button" class="btn food-item">Food</button>
+      </div>
+      <div class="col-md-auto">
+        <button type="button" class="btn food-item">Food</button>
+      </div>
+      <div class="col-md-auto">
+        <button type="button" class="btn food-item">Food</button>
+      </div>
+      <div class="col-md-auto">
+        <button type="button" class="btn food-item">Food</button>
+      </div>
+      <div class="col-md-auto">
+        <button type="button" class="btn food-item">Food</button>
+      </div>
+      <div class="col-md-auto">
+        <button type="button" class="btn food-item">Food</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -33,3 +127,79 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.container {
+  padding: 0 !important;
+  margin: 0 !important;
+  background-color: #f9f9f9;
+  max-width: 100%;
+}
+
+.btn {
+  background-color: white;
+  border-radius: 100px;
+  box-shadow: 0px 3px 3px #bdbdbd;
+}
+
+.tool-bar {
+  margin: 0 !important;
+  flex-wrap: wrap;
+  height: fit-content;
+  display: flex;
+  justify-content: space-between;
+}
+
+.form-outline {
+  padding-right: 20px;
+}
+.form-control {
+  border: none;
+  border-radius: 100px !important;
+  box-shadow: 0px 3px 3px #bdbdbd;
+  display: inline-block;
+}
+
+.tool-bar-container {
+  display: flex;
+  padding: 20px;
+}
+
+.btn-floating {
+  border-radius: 50%;
+  height: fit-content;
+  align-self: center;
+}
+
+.type-of-food {
+  width: 100px;
+  height: 100px;
+  border-radius: 15px;
+  text-align: center;
+  line-height: 150px;
+  background-color: lightgrey;
+  margin: 15px;
+}
+
+.food-item {
+  width: 150px;
+  height: 200px;
+  border-radius: 15px;
+  text-align: left;
+  line-height: 300px;
+  font-size: 2rem;
+  color: white;
+  background-image: linear-gradient(
+      to bottom,
+      rgb(255 255 255 / 0%),
+      rgb(0 0 0 / 73%)
+    ),
+    url("../assets/food.png");
+  margin: 15px;
+}
+
+.col-md-auto {
+  padding: 0;
+  margin: 0;
+}
+</style>
