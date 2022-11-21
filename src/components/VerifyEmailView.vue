650ed9d765c31ba7cc20965c6c3756ec39eddefb
Nashir Janmohamed <nashirbass@gmail.com>
2022-11-21 13:46:10 -0500

    Fix styling on verification success

--- src/components/VerifyEmailView.vue
+++ src/components/VerifyEmailView.vue
@@ -179,6 +179,7 @@ export default {
 
       this.$store.dispatch("auth/confirmVerificationCode", user).then(
         () => {
+          this.loading = false;
           this.successMessage = "Verification succeeded! Please log in.";
         },
         (error) => {
