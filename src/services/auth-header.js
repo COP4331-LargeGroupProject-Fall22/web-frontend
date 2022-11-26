499bceead61b27bf60c1525fa77602dce8d38f37
Nashir Janmohamed <nashirbass@gmail.com>
2022-11-25 20:09:20 -0500

    Fix auth header to use correct key

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -1,8 +1,9 @@
 export default function authHeader() {
     let user = JSON.parse(localStorage.getItem('user'));
-  
+
+    // TODO(add num): Refresh JWT token if needed
     if (user && user.accessToken) {
-      return { 'x-access-token': user.accessToken };       // for Node.js Express back-end
+      return { 'Authorization': user.accessToken };       // for Node.js Express back-end
     } else {
       return {};
     }
